e797d282a55b5d2da5465f687b3a3b7a7a52f82e
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-05 16:12:21 +1100

    imp(WebRequester): Add GetRawAsync and PostRawAsync methods

---
+++ src/FoodDataCentral.NET/WebRequester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FoodDataCentral
+{
+    internal class WebRequester : IRequester
+    {
+        private readonly HttpClient client;
+
+        public WebRequester() : this(new HttpClient()) { }
+
+        public WebRequester(HttpClient httpClient)
+        {
+            client = httpClient;
+        }
+
+        public async Task<string> GetRawAsync(string url)
+        {
+            return await client.GetStringAsync(url);
+        }
+
+        public Task<T> GetAsync<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PostRawAsync(string url, string body)
+        {
+            var content = new StringContent(body, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Task<T> PostAsync<T>(string url, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
