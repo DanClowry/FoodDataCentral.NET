6837a91c886e5ab7daeb71ebb5d430be597fb8cc
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-05 15:51:23 +1100

    test: Add WebRequester tests

---
+++ src/FoodDataCentral.NET.Tests/WebRequestTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Xunit;
+using FoodDataCentral;
+using Xunit.Abstractions;
+using System.IO;
+
+namespace FoodDataCentral.Tests
+{
+    public class WebRequestTests
+    {
+        [Fact]
+        public async void GetRawAsync_MockFoodEndpoint_ReturnsJsonString()
+        {
+            string sampleJson = File.ReadAllText("Data/BigMacFood.json");
+            var mockClient = new HttpClient(MockHttpMessageHandlerFactory("https://api.nal.usda.gov/fdc/v1/170720?api_key=DEMO_KEY", sampleJson));
+            IRequester webRequester = new WebRequester(mockClient);
+
+            string returnedString = await webRequester.GetRawAsync("https://api.nal.usda.gov/fdc/v1/170720?api_key=DEMO_KEY");
+
+            Assert.Equal(sampleJson, returnedString);
+        }
+
+        [Fact]
+        public async void PostRawAsync_MockSearchEndpoint_ReturnsJsonString()
+        {
+            string sampleJson = File.ReadAllText("Data/BigMacSearch.json");
+            var mockClient = new HttpClient(MockHttpMessageHandlerFactory("https://api.nal.usda.gov/fdc/v1/search?api_key=DEMO_KEY", sampleJson));
+            IRequester webRequester = new WebRequester(mockClient);
+
+            string returnedString = await webRequester.PostRawAsync("https://api.nal.usda.gov/fdc/v1/search?api_key=DEMO_KEY", "{\"generalSearchInput\": \"big mac\"}");
+
+            Assert.Equal(sampleJson, returnedString);
+        }
+
+        /// <summary>
+        /// Creates a mock HttpMessageHandler to be used in a HttpClient
+        /// </summary>
+        /// <param name="requestUri">The URI to match incoming requests with</param>
+        /// <param name="returnValue">The value returned to the HttpClient</param>
+        /// <returns>A HttpMessageHandler object</returns>
+        /// <example><code>var handler = MockHttpMessageHandlerFactory("https://example.com", "Return this string")</code></example>
+        private HttpMessageHandler MockHttpMessageHandlerFactory(string requestUri, string returnValue)
+        {
+            var messageHandler = new Mock<HttpMessageHandler>();
+
+            messageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.Is<HttpRequestMessage>(message => message.RequestUri == new Uri(requestUri)), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(returnValue) }).Verifiable();
+
+            return messageHandler.Object;
+        }
+    }
+}
