2b1faca586992895a256e38365f71c1abe9a49df
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-05 11:31:56 +1100

    imp: Add request interface

---
+++ src/FoodDataCentral.NET/IRequester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDataCentral
+{
+    public interface IRequester
+    {
+        Task<string> GetRawAsync(string url);
+
+        Task<T> GetAsync<T>(string url);
+
+        Task<string> PostRawAsync(string url, string body);
+
+        Task<T> PostAsync<T>(string url, string body);
+    }
+}
