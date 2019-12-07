3159ca5891ae2b6cea8cc8bdb5a6a7f159e29395
Daniel Clowry <daniel.clowry2001@gmail.com>
2019-12-07 11:13:52 +1100

    feat: Add food details endpoint controller

---
+++ src/FoodDataCentral.NET/Controllers/FoodController.cs
@@ -0,0 +1,22 @@
+﻿using FoodDataCentral.Models;
+using System.Threading.Tasks;
+
+namespace FoodDataCentral.Controllers
+{
+    internal class FoodController
+    {
+        private readonly IRequester requester;
+        private readonly string apiKey;
+
+        public FoodController(IRequester requester, string apiKey)
+        {
+            this.requester = requester;
+            this.apiKey = apiKey;
+        }
+
+        public async Task<Food> GetFoodById(int id)
+        {
+            return await requester.GetAsync<Food>("https://api.nal.usda.gov/fdc/v1/" + id + "?api_key=" + apiKey);
+        }
+    }
+}
